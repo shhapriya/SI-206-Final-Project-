bb7f6bfb560f760c448c5373f907a058f972e12e
dharajp <dharajp@umich.edu>
2024-04-17 19:42:31 -0400

    another shazam change

--- shazam.py
+++ shazam.py
@@ -9,17 +9,15 @@ import os
 import sqlite3
 from bs4 import BeautifulSoup
 
-# USE beautiful soup for this 
+# USE beautiful soup for this
 # do requests.get
 # read from
 # create database
 
-
-
 def create_database():
     conn = sqlite3.connect('shazam_top_200.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS songs
+    c.execute('''CREATE TABLE IF NOT EXISTS top_songs
                  (rank INTEGER, title TEXT, artist TEXT)''')
     conn.commit()
     conn.close()
@@ -28,12 +26,13 @@ def insert_song(rank, title, artist):
     try:
         conn = sqlite3.connect('shazam_top_200.db')
         c = conn.cursor()
-        c.execute("INSERT INTO songs VALUES (?, ?, ?)", (rank, title, artist))
+        c.execute("INSERT INTO top_songs VALUES (?, ?, ?)", (rank, title, artist))
         conn.commit()
         conn.close()
         print(f"Inserted: Rank: {rank}, Title: {title}, Artist: {artist}")
     except sqlite3.Error as e:
         print("Error inserting data:", e)
+
 
 def scrape_shazam_top_200_US():
     url = 'https://www.shazam.com/charts/top-200/united-states'
@@ -42,17 +41,14 @@ def scrape_shazam_top_200_US():
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         
-        # Find the container holding the song information
-        song_container = soup.find('ol', class_='charttracks')
+        song_container = soup.findall('div', class_='SongItem-module_card__pLL-')
         
         if song_container:
-            # Extract song information from each list item
-            for item in song_container.find_all('li', class_='overflow-hidden'):
-                rank = int(item.find('div', class_='shz-col shz-col--rank').text.strip())
-                title = item.find('div', class_='shz-col shz-col--track').text.strip()
-                artist = item.find('div', class_='shz-col shz-col--artist').text.strip()
+            for item in song_container.find_all('li', class_='page_songItem__lAdHy'):
+                rank = int(item.find('div', class_='SongItem-module_rankingNumber__3oDWK').text.strip())
+                title = item.find('div', class_='common_link__7If7r').text.strip()
+                artist = item.find('div', class_='SongItem-module_metadataLine__7Mm6B').text.strip()
                 
-                # Insert song into database
                 insert_song(rank, title, artist)
         else:
             print("Couldn't find song container.")
@@ -60,11 +56,9 @@ def scrape_shazam_top_200_US():
         print("Failed to retrieve webpage.")
 
 
-def main():
-    # Create database
-    create_database()
 
-    # Scrape and insert top 200 US songs
+def main():
+    create_database()
     scrape_shazam_top_200_US()
 
 if __name__ == "__main__":
