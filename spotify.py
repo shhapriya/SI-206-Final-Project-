6207ad58c92732e55740132b5091b2f6b2162d5c
nethravijay <nethrav@umich.edu>
2024-04-24 21:41:19 -0400

    25

--- spotify.py
+++ spotify.py
@@ -7,20 +7,18 @@ import sqlite3
 from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-
-# Initialize Spotipy with OAuth2 authentication
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="84c4c33c4693466f985310abe2bf7abb",
                                                client_secret="875adb71f2d343478342fcdc58f85f27",
-                                               redirect_uri= "http://localhost:8888/callback",
+                                               redirect_uri="http://localhost:8888/callback",
                                                scope="user-library-read"))
 
 def connect_to_database(db):
-   conn = sqlite3.connect(db)
-   cur = conn.cursor()
-   return conn, cur
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+    return conn, cur
 
 def create_table(cur):
-    cur.execute('''CREATE TABLE IF NOT EXISTS Songs (
+    cur.execute('''CREATE TABLE IF NOT EXISTS top100songs (
                     id INTEGER PRIMARY KEY,
                     name TEXT,
                     artist TEXT,
@@ -31,29 +29,46 @@ def create_table(cur):
                 )''')
 
 def insert_song(cur, conn, name, artist, album, release_year, length, popularity):
-    cur.execute('''INSERT INTO Songs (name, artist, album, release_year, length, popularity) 
+    cur.execute('''INSERT INTO top100songs (name, artist, album, release_year, length, popularity) 
                    VALUES (?, ?, ?, ?, ?, ?)''', (name, artist, album, release_year, length, popularity))
     conn.commit()
 
-def fetch_and_insert_songs(cur, conn, playlistid):
-    results = sp.current_user_saved_tracks()
-    for idx, item in enumerate(results['items']):
-        track = item['track']
-        name = track['name']
-        artist = track['artists'][0]['name']
-        album = track['album']['name']
-        release_year = int(track['album']['release_date'][:4])
-        length = track['duration_ms'] // 1000  # Convert milliseconds to seconds
-        popularity = track['popularity']
-        insert_song(cur, conn, name, artist, album, release_year, length, popularity)
+
+def fetch_and_insert_songs(cur, conn, playlist_url, offset):
+    # Extract playlist ID from the URL
+    playlist_id = playlist_url.split('/')[-1].split('?')[0]
+    
+    try:
+        # Fetch tracks from the specified playlist with the specified offset
+        results = sp.playlist_tracks(playlist_id, limit=25, offset=offset)
+        
+        for idx, item in enumerate(results['items']):
+            track = item['track']
+            name = track['name']
+            artist = track['artists'][0]['name']
+            album = track['album']['name']
+            release_year = int(track['album']['release_date'][:4])
+            length = track['duration_ms'] // 1000  # Convert milliseconds to seconds
+            popularity = track['popularity']
+            insert_song(cur, conn, name, artist, album, release_year, length, popularity)
+            
+    except Exception as e:
+        print(f"Error fetching tracks from the playlist: {e}")
 
 def main():
-   db_path = 'songs.db'
-   conn, cur = connect_to_database(db_path)
-   create_table(cur)
-   playlistid = '1GwPMCU8GrbSSBYtpA5zLB?si=273bc64e2c004df1'
-   fetch_and_insert_songs(cur, conn, playlistid)
-   conn.close()
+    db_path = 'songs.db'
+    conn, cur = connect_to_database(db_path)
+    create_table(cur)
+    playlist_url = 'https://open.spotify.com/playlist/6UeSakyzhiEt4NB3UAd6NQ?si=970c8c3a7d894fa4'
+    
+    offset = 0
+    total_tracks = sp.playlist_tracks(playlist_url)['total']
+    
+    while offset < total_tracks:
+        fetch_and_insert_songs(cur, conn, playlist_url, offset)
+        offset += 25
+    
+    conn.close()
 
 if __name__ == "__main__":
     main()
