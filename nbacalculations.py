32f5aff39fe9a29fc4e33911394205eba5001206
nethravijay <nethrav@umich.edu>
2024-04-30 11:21:50 -0400

    made visualizations

--- nbacalculations.py
+++ nbacalculations.py
@@ -30,6 +30,64 @@ def get_avg_dco(cur):
     avg_dco = cur.fetchone()[0]
     return avg_dco
 
+def get_corr_exp_dco(cur):
+    cur.execute("SELECT depthchartorder, experience FROM players WHERE depthchartorder IS NOT NULL AND experience IS NOT NULL")
+    data = cur.fetchall()
+
+    depthchartorder = []
+    experience = []
+    for row in data:
+        depthchartorder.append(row[0])
+        experience.append(row[1])
+
+    corr_coef = np.corrcoef(depthchartorder, experience)[0, 1]
+    return corr_coef
+def get_sal_exp_corr(cur):
+    cur.execute("SELECT salary, experience FROM players  WHERE experience IS NOT NULL")
+    data = cur.fetchall()
+
+    salary = []
+    experience = []
+    for row in data:
+        salary.append(row[0])
+        experience.append(row[1])
+
+    corr_coef = np.corrcoef(salary, experience)[0, 1]
+    return corr_coef
+
+def exp_vs_sal(cur):
+    cur.execute("SELECT experience, salary FROM players WHERE experience IS NOT NULL AND salary IS NOT NULL")
+    data = cur.fetchall()
+    experience = []
+    salary = []
+    for row in data:
+        experience.append(row[0])
+        salary.append(row[1])
+    avg_experience = get_avg_experience(cur)
+    slope, intercept = np.polyfit(experience, salary, 1)
+    regression_line = [slope * x + intercept for x in experience]
+    plt.scatter(experience, salary, color='pink', label= 'Data')
+    plt.plot(experience, regression_line, color='magenta', label='Regression Line')
+    plt.scatter(avg_experience, np.mean(salary), color='red', label='Average', marker='x', s=100)
+    plt.title('Average Experience vs Salary')
+    plt.xlabel('Experience')
+    plt.ylabel('Salary')
+    plt.show()
+
+def salarygraph(cur):
+    cur.execute("SELECT salary FROM players WHERE salary IS NOT NULL")
+    data = cur.fetchall()
+    salaries = []
+    for row in data:
+        salaries.append(row[0])
+    plt.figure(figsize=(10, 6))  
+    plt.bar(range(len(salaries)), salaries, color='orange', edgecolor='gray')
+    plt.title('Salary Distribution')
+    plt.xlabel('Player')
+    plt.ylabel('Salary')
+    plt.show()
+
+   
 
 
 def main():
@@ -40,10 +98,15 @@ def main():
     average_height = get_height_avg(cur)
     average_experience = get_avg_experience(cur)
     average_depth_chart_order = get_avg_dco(cur)
+    correlation_salary_experience = get_sal_exp_corr(cur)
+    correlation_depth_order_experience = get_corr_exp_dco(cur)
+    exp_vs_sal(cur)
+    salarygraph(cur)
 
 
 
     cur.close()
+    conn.close()
 
    
 if __name__ == "__main__":
